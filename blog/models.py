9fa22a629e99dbe9edbcfbfc9b992c21e8c85966
Colin Shaw <colin.shaw400@outlook.com>
2022-08-24 17:21:18 +0000

    Add status tuples to be able to publish posts and keep drafts to seperate them before rendering

--- blog/models.py
+++ blog/models.py
@@ -2,3 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+#declaring tuples to be able to post drafts and publish posts
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
