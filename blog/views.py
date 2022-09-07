555ade15d589e6cc5cddd9af4b374264a8c33e46
Colin Shaw <colin.shaw400@outlook.com>
2022-09-07 18:26:57 +0000

    Add mapping from urls.py to views.py

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post
 # Create your views here.
 # subclass ListView allows rendering a list with objects of models
 class ListPost(generic.ListView):
-    # filter allows only published statuses to be showen
+    # filter allows only published statuses to be showen on the front end
     query = Post.objects.filter(status=1).order_by('-created_on')# - before created on allows latest post be shown
     template = 'index.html'
 
--- filmblog/urls.py
+++ filmblog/urls.py
@@ -18,4 +18,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.ListPost.as_view(), name='home'),
+    path('<slug:slug>/', views.DetailsPost.as_view(), name='details_post'),
 ]
