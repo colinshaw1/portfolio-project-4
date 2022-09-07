2d366cb052a0f3a4cff0b0c1bb82a7b8d0d941b6
Colin Shaw <colin.shaw400@outlook.com>
2022-09-07 18:36:15 +0000

    add import for to include the path for the new urls.py file to allow the requests be handled by the filmblog app

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    # '' represents empty string and returns an result from ListPost
     path('', views.ListPost.as_view(), name='home'),
+    # <slug> in the brackets captures the values from the url and returns the post details
     path('<slug:slug>/', views.DetailsPost.as_view(), name='details_post'),
 ]
--- filmblog/urls.py
+++ filmblog/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.ListPost.as_view(), name='home'),
-    path('<slug:slug>/', views.DetailsPost.as_view(), name='details_post'),
+    # allows requests be handles by the filmblog app
+    path('', include(blog.urls)),
 ]
