793d230c6bab794aaa1c5560a5c5837b2d17f9e4
Colin Shaw <colin.shaw400@outlook.com>
2022-09-07 17:34:16 +0000

    Add post admin class to be able to uses djangos features for searching, filtering and displaying lists

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 #import from models file for post method
 from .models import Post
 
+
+# add class for making admin dashbord have filters, searchs and list fileds using djangos bnuilt in methods
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('film_title', 'slug', 'status', 'created_on')
+    list_filter = ('status',)
+    search_fields = ['film_title', 'content']
+    prepopulated_fields = {'slug':('film_title',)}
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
