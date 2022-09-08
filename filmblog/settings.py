bd4a4beb51c9b41d4fd16186515d8a29b6385096
Colin Shaw <colin.shaw400@outlook.com>
2022-09-08 17:51:15 +0000

    Add import os and TEMPLATES_DIRS to templates in DIRS

--- filmblog/settings.py
+++ filmblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,7 +60,8 @@ ROOT_URLCONF = 'filmblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # Add templates directory to templates
+        'DIRS': [TEMPLATES_DIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
