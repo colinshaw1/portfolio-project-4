1d527f023b4b82f3c9289f8b08529dc0cd3a1c7d
Colin Shaw <colin.shaw400@outlook.com>
2022-09-09 20:44:49 +0000

    Add about page link in a sidebar to on index.html page.

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     # '' represents empty string and returns an result from ListPost
-    path('', views.ListPost.as_view(), name='home'),
+    path('', views.PostList.as_view(), name='home'),
     # <slug> in the brackets captures the values from the url and returns the post details
-    path('<slug:slug>/', views.DetailsPost.as_view(), name='details_post'),
+    path('<slug:slug>/', views.PostDetails.as_view(), name='details_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,11 @@ from .models import Post
 
 # Create your views here.
 # subclass ListView allows rendering a list with objects of models
-class ListPost(generic.ListView):
+class PostList(generic.ListView):
     # filter allows only published statuses to be showen on the front end
     queryset = Post.objects.filter(status=1).order_by('-created_on')# - before created on allows latest post be shown
     template_name = 'index.html'
 
-class DetailsPost(generic.DetailView):
+class PostDetails(generic.DetailView):
     model = Post
     template_name = 'details_post.html'
