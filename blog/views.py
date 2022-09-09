765e39d6b2482ca6a3c7503acdf44d3ca6fcc038
Colin Shaw <colin.shaw400@outlook.com>
2022-09-09 21:07:39 +0000

    add details_post.html page to show content when film post is clicked on

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,11 @@ from .models import Post
 
 # Create your views here.
 # subclass ListView allows rendering a list with objects of models
-class PostList(generic.ListView):
+class ListPost(generic.ListView):
     # filter allows only published statuses to be showen on the front end
     queryset = Post.objects.filter(status=1).order_by('-created_on')# - before created on allows latest post be shown
     template_name = 'index.html'
 
-class PostDetails(generic.DetailView):
+class DetailsPost(generic.DetailView):
     model = Post
     template_name = 'details_post.html'
