f935b16c350c921c6fe625fddedb25a5a511e348
Colin Shaw <colin.shaw400@outlook.com>
2022-12-07 20:32:21 +0000

    Add alluath for authorisation

--- filmblog/urls.py
+++ filmblog/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     # allows requests be handles by the filmblog app
     path('', include('blog.urls')),
+    # allows a path the allauth urls for authurisation
+    path('accounts/', include('allauth.urls')),
 ]
