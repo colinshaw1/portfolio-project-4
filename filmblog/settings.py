1114af33aa57c80670c6633ca0cd6a6d9e9a09d1
Colin Shaw <colin.shaw400@outlook.com>
2022-12-07 21:03:31 +0000

    Add crispy forms and add cripsy forms app to settings file

--- filmblog/settings.py
+++ filmblog/settings.py
@@ -53,13 +53,17 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'django.contrib.staticfiles',
     'django_summernote',
+    'crispy_forms',
     'blog',
+
 ]
 
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
