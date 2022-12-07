93ef899de5ce8ee7b1abcd44a778b2b1983bb6d2
Colin Shaw <colin.shaw400@outlook.com>
2022-12-07 21:11:16 +0000

    Add crispy forms to views and forms files

--- blog/forms.py
+++ blog/forms.py
@@ -5,9 +5,4 @@ from django import forms
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('name', 'body')
-
-# class ContactForm(forms.ModelForm):
-#     class Meta:
-#         model = Contact
-#         fields = ('name', 'email')
+        fields = ('body',)
