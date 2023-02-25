3dee5022c46956550261876c30e320e5d7145de9
Colin Shaw <colin.shaw400@outlook.com>
2023-02-25 18:07:20 +0000

    add form gorups and control to style form for adding film review

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Comment, Post
 from django import forms
 
 #class to create a model to hanbdle form processing and validation
@@ -6,3 +6,18 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+# form fields for ous post model
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('film_title','blogger', 'slug', 'director', 'actor', 'content')
+        # creates a dictonary for the form using bootstrap form control
+        widget = {
+            'film_title': form.TextInput(attrs={'class': 'form-control'})
+            'blogger': form.Select(attrs={'class': 'form-control'})
+            'slug': form.TextInput(attrs={'class': 'form-control'})
+            'director': form.TextInput(attrs={'class': 'form-control'})
+            'actor': form.TextInput(attrs={'class': 'form-control'})
+            'content': form.TextArea(attrs={'class': 'form-control'})
+        }
