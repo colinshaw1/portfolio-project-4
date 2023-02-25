010e1cc45c14cc2e2b93a55c73211daf0f839ea2
Colin Shaw <colin.shaw400@outlook.com>
2023-02-25 19:27:10 +0000

    add url for edit post

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('add_post/', views.AddPostView.as_view(), name="add_post"),
     # <slug> in the brackets captures the values from the url and returns the post details
     path('<slug:slug>/', views.PostDetail.as_view(), name='details_post'),
+    path('<slug:slug>/edit', views.EditPost.as_view(), name='edit_post'),
     path('likes/<slug:slug>', views.PostLike.as_view(), name="post_like"),
     
 ]
