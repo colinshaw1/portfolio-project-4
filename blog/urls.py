c340402c5692140b78b5759d9aed61b0eff196fb
Colin Shaw <colin.shaw400@outlook.com>
2023-02-25 16:46:48 +0000

    add nav item to the correct place

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     # <slug> in the brackets captures the values from the url and returns the post details
     path('<slug:slug>/', views.PostDetail.as_view(), name='details_post'),
     path('likes/<slug:slug>', views.PostLike.as_view(), name="post_like"),
-    path('add_post/', views.AddPostView.as_view(), name= "add_post")
+    path('add_post/', views.AddPostView.as_view(), name="add_post")
 ]
