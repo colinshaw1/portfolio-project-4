da8a2414acde20eb2ec9bb52502648e780f66781
Colin Shaw <colin.shaw400@outlook.com>
2023-02-25 16:49:11 +0000

    add updated urls file to correct file order

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,9 @@ from django.urls import path
 urlpatterns = [
     # '' represents empty string and returns an result from ListPost
     path('', views.ListPost.as_view(), name='home'),
+    path('add_post/<slug:slug>', views.AddPostView.as_view(), name="add_post"),
     # <slug> in the brackets captures the values from the url and returns the post details
     path('<slug:slug>/', views.PostDetail.as_view(), name='details_post'),
     path('likes/<slug:slug>', views.PostLike.as_view(), name="post_like"),
-    path('add_post/<slug:slug>', views.AddPostView.as_view(), name="add_post")
+    
 ]
