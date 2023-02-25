62d3f071171ff9e3eb066695269f2af5bbda6f5b
Colin Shaw <colin.shaw400@outlook.com>
2023-02-25 17:20:27 +0000

    add form fields to add review

--- blog/views.py
+++ blog/views.py
@@ -91,4 +91,4 @@ class AddPostView(generic.CreateView):
     model = Post
     template_name = "add_post.html"
     # allows all fields to be shown on page
-    fields = "__all__"
+    fields = ('film_title', 'director', 'actor', 'content')
