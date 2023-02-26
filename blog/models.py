e319c01eb99447f5e7b7e4a0d0348cc40fe0c8e5
Colin Shaw <colin.shaw400@outlook.com>
2023-02-26 08:14:48 +0000

    Add changes to comment model

--- blog/models.py
+++ blog/models.py
@@ -50,17 +50,15 @@ class Post(models.Model):
     def number_of_likes(self):
         return self.likes.count()
 
-#class for commetns model to post with correct infromation
+#class for comments model to post with correct infromation
 class Comment(models.Model):
     post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
+    name = models.CharField(max_length=255)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['created_on']
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return f"Comment {self.post.film_title} by {self.post.name}"
