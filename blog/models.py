167252dac4ef9a257159afb86de64b480f8c5d8e
Colin Shaw <colin.shaw400@outlook.com>
2023-02-26 10:00:33 +0000

    Add fixes to models page

--- blog/models.py
+++ blog/models.py
@@ -63,4 +63,4 @@ class Comment(models.Model):
         ordering = ['created_on']
 
     def __str__(self):
-        return f"Comment {self.post.film_title} by {self.post.name}"
+        return f"Comment {self.body} by {self.name}"
