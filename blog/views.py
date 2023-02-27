34ebc8a45deae33d8a25e9ac2dd99b0141443b7e
Colin Shaw <colin.shaw400@outlook.com>
2023-02-27 19:28:14 +0000

    add update to comment view

--- blog/views.py
+++ blog/views.py
@@ -24,56 +24,56 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        # gets the comments attached to a post
-        comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
-        # check if liked post by user id
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
+    #     # gets the comments attached to a post
+    #     comments = post.comments.filter(approved=True).order_by("-created_on")
+    #     liked = False
+    #     # check if liked post by user id
+    #     if post.likes.filter(id=self.request.user.id).exists():
+    #         liked = True
 
-        return render(
-            request,
-            "details_post.html",
-            {
-                "post": post,
-                "comments": comments,
-                "liked": liked,
-                "comment_form": CommentForm()
-            },
-        )
-    # same as get method above
-    def post(self, request, slug, *args, **kwargs):
+    #     return render(
+    #         request,
+    #         "details_post.html",
+    #         {
+    #             "post": post,
+    #             "comments": comments,
+    #             "liked": liked,
+    #             "comment_form": CommentForm()
+    #         },
+    #     )
+    # # same as get method above
+    # def post(self, request, slug, *args, **kwargs):
 
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
-        # creating a new variable called commentform
-        comment_form = CommentForm(data=request.POST)
-        # gets the data from the form
-        # see if the user leaving the comment is valid
-        if comment_form.is_valid():
-            comment_form.instance.email = request.user.email
-            comment_form.instance.name = request.user.username
-            comment = comment_form.save(commit=False)
-            comment.post = post
-            comment.save()
-        else:
-            comment_form = CommentForm()
+    #     queryset = Post.objects.filter(status=1)
+    #     post = get_object_or_404(queryset, slug=slug)
+    #     comments = post.comments.filter(approved=True).order_by("-created_on")
+    #     liked = False
+    #     if post.likes.filter(id=self.request.user.id).exists():
+    #         liked = True
+    #     # creating a new variable called commentform
+    #     comment_form = CommentForm(data=request.POST)
+    #     # gets the data from the form
+    #     # see if the user leaving the comment is valid
+    #     if comment_form.is_valid():
+    #         comment_form.instance.email = request.user.email
+    #         comment_form.instance.name = request.user.username
+    #         comment = comment_form.save(commit=False)
+    #         comment.post = post
+    #         comment.save()
+    #     else:
+    #         comment_form = CommentForm()
 
-        return render(
-            request,
-            "details_post.html",
-            {
-                "post": post,
-                "comments": comments,
-                "commented": True,
-                "comment_form": comment_form,
-                "liked": liked
-            },
-        )
+    #     return render(
+    #         request,
+    #         "details_post.html",
+    #         {
+    #             "post": post,
+    #             "comments": comments,
+    #             "commented": True,
+    #             "comment_form": comment_form,
+    #             "liked": liked
+    #         },
+    #     )
 
 class PostLike(View):
     
